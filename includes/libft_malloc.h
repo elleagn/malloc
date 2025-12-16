0a20bc5c77106851328700679c2ffcd829526aad
Gaelle Ozon <gozon@student.42.fr>
2025-12-16 08:55:24 +0100

    Start to define structs

--- includes/libft_malloc.h
+++ includes/libft_malloc.h
@@ -3,6 +3,18 @@
 
 #include <stddef.h>
 
+#define PREV_INUSE 1
+
+typedef struct s_chunk
+{
+    size_t prev_size;
+    size_t size;
+}   t_chunk;
+
+typedef struct arena {
+    t_chunk *heap;
+};
+
 void    *malloc(size_t size);
 
 #endif
