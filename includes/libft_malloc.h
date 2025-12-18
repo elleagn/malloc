c642a46d438c500de46bc2016fd0d2b989d9eb9d
gozon <gozon@student.42.fr>
2025-12-18 09:34:20 +0100

    Add comments + minimum chunk size

--- includes/libft_malloc.h
+++ includes/libft_malloc.h
@@ -9,6 +9,66 @@
 
 #define HEAP_HEADER_SIZE 16
 
+/**
+ * Reimplementation of a dynamic memory allocator.
+ * The heap and chunk structure and their documentation are inspired by glibc malloc
+ * (https://github.com/lattera/glibc/blob/master/malloc/malloc.c).
+ *
+ * Alignment:
+ *  - Allocated memory is always 8 bytes aligned
+ */
+
+ /**
+  * HEAP(S) STRUCTURE
+  */
+
+/**
+ * Chunks of memory are delimited by a 'boundary tag' (ie the size of free chunks
+ * is stored both at the beginning and at the end of each chunk). The last 3 bytes
+ * are used as flags (to know if previous chunk was in use).
+ *
+ * Allocated chunks look like this:
+ *
+ *  chunk-> +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *	    |             Size of previous chunk, if unallocated (P clear)  |
+ *	    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *	    |             Size of chunk, in bytes                     |0|0|P|
+ *     ptr-> +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *	    |             User data                                         .
+ *	    .                                                               .
+ *	    .                                                               |
+ *   nextchunk-> +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *	    |             (size of chunk, but used for application data)    |
+ *	    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *	    |             Size of next chunk, in bytes                |0|0|1|
+ *	    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *
+ * ptr is the address returned to the user.
+ * Free chunks are put in bins (circular double linked lists) to facilitate
+ * insertion / removal. They look like this:
+ *
+ *   chunk-> +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *	    |             Size of previous chunk, if unallocated (P clear)  |
+ *	    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *      |             Size of chunk, in bytes                     |0|0|P|
+ *     ptr-> +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *	    |             Forward pointer to next chunk in list             |
+ *	    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *	    |             Back pointer to previous chunk in list            |
+ *	    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *	    |             Unused space (may be 0 bytes long)                .
+ *	    .                                                               .
+ *	    .                                                               |
+ *  nextchunk-> +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *      |             Size of chunk, in bytes                           |
+ *	    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *	    |             Size of next chunk, in bytes                |0|0|0|
+ *	    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *
+ * PREV_INUSE flag (P) is stored in the lowest byte of the size (which is always
+ * a multiple of 8). It is 1 if the previous chunk is used and 0 if it is not.
+ */
+
 typedef struct s_chunk {
     size_t          prev_size;
     size_t          size;
@@ -28,6 +88,10 @@ typedef struct s_arena {
 extern t_arena arena;
 
 t_chunk *find_fitting_chunk(size_t size);
+
+/**
+ * 
+ */
 t_chunk *resize_chunk(t_chunk *chunk, size_t size);
 void    add_chunk(t_chunk *chunk, t_chunk **bin);
 void    remove_chunk(t_chunk *chunk, t_chunk **bin);
--- srcs/chunks.c
+++ srcs/chunks.c
@@ -4,6 +4,9 @@
 t_chunk *find_fitting_chunk(size_t size) {
 
     t_chunk *current_chunk = arena.heap->bin;
+    if (size < 16) {
+        size = 16;
+    }
 
     while (current_chunk != NULL) {
         if (current_chunk->size - 8 >= size) {
@@ -18,15 +21,19 @@ t_chunk *find_fitting_chunk(size_t size) {
 
 t_chunk *resize_chunk(t_chunk *chunk, size_t size) {
 
-    size_t usable_size = size;
+    size_t usable_size = size >= 16 ? size : 16;
     if (size % 8 != 0) {
         usable_size = size - size % 8 + 8;
     }
     size_t flags = chunk->size & 7;
     size_t old_size = chunk->size - flags;
+    size_t new_size = usable_size + 8;
+    if (old_size - new_size < 32) {
+        return (chunk);
+    }
 
-    chunk->size = usable_size + 8 + flags;
-    size_t remainder_size = old_size - usable_size - 8 + PREV_INUSE;
+    chunk->size = new_size + flags;
+    size_t remainder_size = old_size - new_size + PREV_INUSE;
     t_chunk *remainder_chunk = (t_chunk *)((uintptr_t)chunk + chunk->size - flags);
     remainder_chunk->size = remainder_size;
     size_t *end_tag = (size_t *)((uintptr_t)remainder_chunk + remainder_chunk->size - PREV_INUSE);
