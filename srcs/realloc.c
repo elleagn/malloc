b6f262338f017c0fa125fc4fe92e7ff284971e76
Gaelle Ozon <gozon@student.42.fr>
2026-02-02 13:41:59 +0100

    Add comments

--- srcs/realloc.c
+++ srcs/realloc.c
@@ -8,6 +8,14 @@ int is_in_use(t_chunk *chunk) {
     return (flag);
 }
 
+/**
+ * @brief Check in the following chunks are free, until either the desired size
+ * is reached or a used chunk is found
+ * @todo END OF THE HEAP
+ * @param chunk The chunk after which to look for space
+ * @param size The desired total space (including the beginning chunk)
+ * @return The potential free size
+ */
 size_t find_free_space(t_chunk *chunk, size_t size) {
     t_chunk *current_chunk = (t_chunk *)((uintptr_t)chunk + chunk->size);
     size_t   potential_size = chunk->size - 8;
@@ -20,10 +28,24 @@ size_t find_free_space(t_chunk *chunk, size_t size) {
     return (potential_size);
 }
 
+/**
+ * @brief If there is enough free spaces next to the chunk, expands it to the
+ * desired size. The data contained in the chunk is unaffected. Nothing is
+ * changed if the wasn't enough free space.
+ * @todo chunk expansion
+ * @param chunk The chunk to expand
+ * @param bin The bin of the chunk's bin, to remove the next chunks if need be
+ * @param size The desired size of the new chunk
+ * @return The expanded chunk if enough free space was found, 0 otherwise
+ */
+
 t_chunk *try_expand_chunk(t_chunk *chunk, t_chunk **bin, size_t size) {
     size_t free_space = find_free_space(chunk, size);
 }
 
+/**
+ * @todo DON'T GO BEYOND HEAP + smaller functions
+ */
 void *realloc(void *ptr, size_t size) {
 
     if (ptr == NULL) {
