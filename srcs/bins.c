5bcd6b105d8aa5b0f612c2a61d80f51cebfe02c0
Gaelle Ozon <gozon@student.42.fr>
2026-02-16 08:48:16 +0100

    Better comments for bins

--- srcs/bins.c
+++ srcs/bins.c
@@ -1,5 +1,10 @@
 #include "libft_malloc.h"
 
+/**
+ * @brief Remove chunk from a list of chunks (tiny or small heap).
+ * @param chunk The chunk to remove
+ * @param bin Double pointer to the chunk list
+ */
 void remove_chunk(t_chunk *chunk, t_chunk **bin) {
 
     t_chunk *next = chunk->next_free_chunk;
@@ -17,6 +22,11 @@ void remove_chunk(t_chunk *chunk, t_chunk **bin) {
     }
 }
 
+/**
+ * @brief Add chunk to a lost of chunks (tiny or small heap)
+ * @param chunk Pointer to the chunk to add
+ * @param bin Double pointer to the chunk list
+ */
 void add_chunk(t_chunk *chunk, t_chunk **bin) {
 
     chunk->next_free_chunk = *bin;
