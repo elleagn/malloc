3d5972c360860d147817cd2cf970268985a2f42a
Gaelle Ozon <gozon@student.42.fr>
2026-02-16 10:02:34 +0100

    Better comments for realloc

--- srcs/realloc.c
+++ srcs/realloc.c
@@ -17,9 +17,15 @@ int is_in_use(t_chunk *chunk) {
     return (flag);
 }
 
+/**
+ * @brief Fuses the given chunk with the next consecutive one.
+ * @param chunk The chunk to fuse
+ * @param bin Double pointer to a chunk list from which to remove the right
+ * chunk (that is assumed to be free)
+ */
 void fuse_next_chunk(t_chunk *chunk, t_chunk **bin) {
-    t_chunk *next_chunk =  (t_chunk * )((uintptr_t)chunk + get_chunk_size(chunk));
-    int in_use = next_chunk-> size % 8;
+    t_chunk *next_chunk = (t_chunk *)((uintptr_t)chunk + get_chunk_size(chunk));
+    int      in_use = next_chunk->size % 8;
 
     remove_chunk(next_chunk, bin);
     chunk->size += get_chunk_size(next_chunk);
@@ -39,8 +45,10 @@ void fuse_next_chunk(t_chunk *chunk, t_chunk **bin) {
  */
 
 void try_expand_chunk(t_chunk *chunk, t_chunk **bin, size_t size) {
-    t_chunk *current_chunk = (t_chunk *)((uintptr_t)chunk + get_chunk_size(chunk));
-    while (current_chunk->user_size != 0 && !is_in_use(current_chunk) && size > chunk->size) {
+    t_chunk *current_chunk =
+        (t_chunk *)((uintptr_t)chunk + get_chunk_size(chunk));
+    while (current_chunk->user_size != 0 && !is_in_use(current_chunk) &&
+           size > chunk->size) {
         fuse_next_chunk(chunk, bin);
         current_chunk = (t_chunk *)((uintptr_t)chunk + chunk->size);
     }
@@ -59,12 +67,14 @@ int belong_to_same_heap(size_t size1, size_t size2) {
     return (1);
 }
 
+/**
+ * @brief Tries to reallocate the memory in place
+ * @param chunk chunk to reallocate
+ * @param size desired user size
+ * @return chunk if the chunk can be realloced in place, NULL otherwise
+ */
 t_chunk *try_in_place(t_chunk *chunk, size_t size) {
     t_segment *heap = find_right_segment(chunk);
-    // t_chunk   *next_chunk = (t_chunk *)((uintptr_t)ptr_chunk + ptr_chunk->size);
-
-
-    // Check if we will go past the heap
 
     try_expand_chunk(chunk, &heap->bin, size);
     if (size < get_chunk_size(chunk) - CHUNK_HEADER_SIZE + sizeof(size_t)) {
@@ -77,7 +87,19 @@ t_chunk *try_in_place(t_chunk *chunk, size_t size) {
 }
 
 /**
- * @todo smaller functions
+ * @brief Changes the size of the memory block pointed to by ptr to size bytes.
+ * The contents will be unchanged in the range from the start of the region up
+ * to the minimum of the old and new sizes.  If the new size is larger than the
+ * old size, the added memory will not be initialized.  If ptr is NULL, then the
+ * call is equivalent to malloc(size), for all values of size; if size is equal
+ * to zero, and ptr is not NULL, then the call is equivalent to free(ptr).
+ * Unless ptr is NULL, it must have been returned by an earlier call to
+ * malloc(), or realloc().  If the area pointed to was moved, a free(ptr) is
+ * done.
+ * @param ptr Pointer to resize
+ * @param size size of the new pointer
+ * @return The new pointer (or the old one if the memory could be reallocated in
+ * place)
  */
 void *realloc(void *ptr, size_t size) {
 
@@ -88,38 +110,23 @@ void *realloc(void *ptr, size_t size) {
         free(ptr);
         return (NULL);
     }
-    ft_printf("1\n");
-    // print_heap();
-    ft_printf("\n");
 
-    t_chunk   *new_chunk = NULL;
-    t_chunk   *chunk = (t_chunk *)((uintptr_t)ptr - CHUNK_HEADER_SIZE);
+    t_chunk *new_chunk = NULL;
+    t_chunk *chunk = (t_chunk *)((uintptr_t)ptr - CHUNK_HEADER_SIZE);
 
-     if (belong_to_same_heap(size, get_chunk_size(chunk))) {
+    if (belong_to_same_heap(size, get_chunk_size(chunk))) {
         new_chunk = try_in_place(chunk, size);
     }
 
-    ft_printf("2\n");
-    // print_heap();
-    ft_printf("\n");
-
-    void  *result = NULL;
+    void *result = NULL;
     if (new_chunk != NULL) {
         new_chunk->user_size = size;
         return ((void *)((uintptr_t)new_chunk + CHUNK_HEADER_SIZE));
     } else {
         result = malloc(size);
-        ft_printf("3\n");
-        // print_heap();
-        ft_printf("\n");
-        ft_memmove(result, ptr, chunk->user_size < size ? chunk->user_size : size);
-        ft_printf("4\n");
-        // print_heap();
-        ft_printf("\n");
+        ft_memmove(result, ptr,
+                   chunk->user_size < size ? chunk->user_size : size);
         free(ptr);
-        ft_printf("5\n");
-        // print_heap();
-        ft_printf("\n");
     }
     return (result);
 }
